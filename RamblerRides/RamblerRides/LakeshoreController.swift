ab084ce2786a72167346d5451408fc171001271e
tylerbobella <tjbobella@gmail.com>
2015-11-12 21:23:39 -0600

    checkpoint on segues. need to add navigator segue and start model for API access

---
+++ RamblerRides/RamblerRides/LakeshoreController.swift
@@ -0,0 +1,14 @@
+//
+//  LakeshoreController.swift
+//  RamblerRides
+//
+//  Created by Tyler Bobella on 11/12/15.
+//  Copyright © 2015 LUCCS. All rights reserved.
+//
+
+import UIKit
+
+class LakeshoreController: UIViewController {
+
+
+}
---
+++ RamblerRides/RamblerRides/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  RamblerRides
+//
+//  Created by Tyler Bobella on 11/12/15.
+//  Copyright © 2015 LUCCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var campuses: [UIButton]!
+
+
+    @IBAction func chooseCampus(sender: AnyObject) {
+        
+        // Not 100% sure if "tag" is the right method here.
+        // I would like to pass a keyPath but I believe you need to
+        // Make a subclass of the button to allow this. Not sure if
+        // That is standard iOS development moves or not. Oh well. 
+        switch sender.tag {
+        case 1:
+            self.performSegueWithIdentifier("lakeshore", sender: self)
+        case 2:
+            self.performSegueWithIdentifier("watertower", sender: self)
+        default:
+            break
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "lakeshore" {
+            segue.destinationViewController as? LakeshoreController
+        }
+        
+        if segue.identifier == "watertower" {
+            segue.destinationViewController as? WatertowerController
+        }
+    }
+}
+
