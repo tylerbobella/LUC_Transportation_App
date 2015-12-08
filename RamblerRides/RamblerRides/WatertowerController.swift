155ddc639b0be9d724d7f11b12fe3c42fc58ad7c
clanzer <clanzer@luc.edu>
2015-12-08 11:26:04 -0600

    Added data to the schedule timer, has issues updating itself once the timer reaches 0 though.

--- RamblerRides/RamblerRides/WatertowerController.swift
+++ RamblerRides/RamblerRides/WatertowerController.swift
@@ -12,10 +12,17 @@ class WatertowerController: UIViewController {
 
         let cta = CTA()
     
-    var timer = NSTimer() //make a timer variable, but do do anything yet
+    var timer = NSTimer()
     let timeInterval:NSTimeInterval = 0.01
     let timerEnd:NSTimeInterval = 10.0
-    var timeCount:NSTimeInterval = 10.0
+    let currentDate = NSDate()
+    var timeCount:NSTimeInterval = 5.0
+    let scenario0: NSTimeInterval = 900
+    let scenario1: NSTimeInterval = 1200
+    let scenario2: NSTimeInterval = 2100
+    let scenario3: NSTimeInterval = 3000
+    let scenario4: NSTimeInterval = 3300
+    let scenario5: NSTimeInterval = 3600
     
     @IBOutlet weak var WTCBusTime: UILabel!
     @IBOutlet weak var countingDown: UISwitch!
@@ -26,6 +33,7 @@ class WatertowerController: UIViewController {
         cta.getTrain("watertower") { response, error in
             print(response)
         }
+        changeTime()
         startTimer()
     }
     
@@ -41,29 +49,41 @@ class WatertowerController: UIViewController {
     }
     
     @IBAction func countingDown(sender: UISwitch) {
-        if !timer.valid{
+        if !timer.valid {
             resetTimeCount()
     }
     }
-
+ 
     func stopTimer() {
-        //timerLabel.text = "Timer Stopped"
         timer.invalidate()
     }
     
-   func resetTimer() {
-        timer.invalidate()
-        resetTimeCount()
-        WTCBusTime.text = timeString(timeCount)
+    func changeTime() {
+        if timerScenario() == 0 {
+            timeCount = scenario0 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
+        if timerScenario() == 1 {
+            timeCount = scenario1 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
+        if timerScenario() == 2 {
+            timeCount = scenario2 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
+        if timerScenario() == 3 {
+            timeCount = scenario3 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
+        if timerScenario() == 4 {
+            timeCount = scenario4 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
+        if timerScenario() == 5 {
+            timeCount = scenario5 - Double(currentDate.minute()*60) - Double(currentDate.second())
+        }
     }
     
     func resetTimeCount(){
-        if countingDown.on{
-            timeCount = timerEnd
-           startTimer()
-        } else {
-            timeCount = 0.0
-        }
+        stopTimer()
+        timerScenario()
+        changeTime()
+        startTimer()
     }
     
     func timeString(time:NSTimeInterval) -> String {
@@ -74,23 +94,98 @@ class WatertowerController: UIViewController {
         return String(format:"%02i:%02i.%01i",minutes,Int(seconds),Int(secondsFraction * 10.0))
     }
     
+    // THIS SECTION IS THE TESTS TO SEE WHAT COUNTDOWN TIMER TO USE //
+    
+    func timerScenario() -> Int{
+        if currentDate.hour() < 7 && currentDate.hour() > 8 {
+            return 6
+        }
+        else if currentDate.minute() >= 0 && currentDate.minute() < 15  {
+            return 0
+        }
+        else if currentDate.minute() >= 15 && currentDate.minute() < 20  {
+            return 1
+        }
+       else if currentDate.minute() >= 20 && currentDate.minute() < 35  {
+            return 2
+        }
+        else if currentDate.minute() >= 35 && currentDate.minute() < 40  {
+            return 3
+        }
+        else if currentDate.minute() >= 40 && currentDate.minute() < 55  {
+            return 4
+        }
+        else if currentDate.minute() >= 55 && currentDate.minute() < 60  {
+            return 5
+        }
+        else if currentDate.hour() < 7 && currentDate.hour() > 1 {
+            return 6
+        }
+        return 7
+    }
+    
+    ///////////////////////////////////////////
+    /* This is what happens when each scenario is activated */
+    
     func timerDidEnd(timer:NSTimer){
-        if countingDown.on{
-            //timer that counts down
-            timeCount = timeCount - timeInterval
+        if timerScenario() == 6 {
+            WTCBusTime.text = "The Bus doesn't run now"
+        }
+       else if timerScenario() == 0 {
+           timeCount = timeCount - timeInterval
             if timeCount <= 0 {  //test for target time reached.
-                WTCBusTime.text = "Bus Arrived"
-                resetTimeCount()
+                WTCBusTime.text = "Bus Arriving"
                  countingDown.setOn(false, animated:true)
+                resetTimeCount()
             } else { //update the time on the clock if not reached
                 WTCBusTime.text = "Bus Arriving in: " + timeString(timeCount)
             }
-            
-        } else {
+        }
+       else if timerScenario() == 1 {
+            timeCount = timeCount - timeInterval
+            if timeCount <= 0{  //test for target time reached.
+                WTCBusTime.text = "Bus Leaving"
+                resetTimeCount()
+            } else { //update the time on the clock if not reached
+                WTCBusTime.text = "Bus Leaving in: " + timeString(timeCount)
+            }
+        }
+       else if timerScenario() == 2 {
+            timeCount = timeCount - timeInterval
+            if timeCount <= 0{  //test for target time reached.
+                WTCBusTime.text = "Bus Arriving"
+                countingDown.setOn(false, animated:true)
+                resetTimeCount()
+            } else { //update the time on the clock if not reached
+                WTCBusTime.text = "Bus Arriving in: " + timeString(timeCount)
+            }
+        }
+       else if timerScenario() == 3 {
             timeCount = timeCount - timeInterval
             if timeCount <= 0{  //test for target time reached.
                 WTCBusTime.text = "Bus Leaving"
                 countingDown.setOn(true, animated:true)
+                resetTimeCount()
+            } else { //update the time on the clock if not reached
+                WTCBusTime.text = "Bus Arriving in: " + timeString(timeCount)
+            }
+        }
+      else if timerScenario() == 4 {
+            timeCount = timeCount - timeInterval
+            if timeCount <= 0{  //test for target time reached.
+                WTCBusTime.text = "Bus Arriving"
+                countingDown.setOn(false, animated:true)
+                resetTimeCount()
+            } else { //update the time on the clock if not reached
+                WTCBusTime.text = "Bus Arriving in: " + timeString(timeCount)
+            }
+        }
+      else if timerScenario() == 5 {
+            timeCount = timeCount - timeInterval
+            if timeCount <= 0{  //test for target time reached.
+                WTCBusTime.text = "Bus Leaving"
+                countingDown.setOn(true, animated:true)
+                timerScenario() == 6
                 resetTimeCount()
             } else { //update the time on the clock if not reached
                 WTCBusTime.text = "Bus Leaving in: " + timeString(timeCount)
